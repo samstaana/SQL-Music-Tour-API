9386d3e70978136239adc9cb052506157eb2a824
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-08-28 23:30:12 -0700

    added attributes to band.js

--- models/band.js
+++ models/band.js
@@ -1,15 +1,24 @@
-// DEPENDENCIES
-const { Sequelize, DataTypes, Model } = require('sequelize')
-
-// MODEL
-class Band extends Model{}
-
-Band.init({
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Band extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Band.init({
     band_id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
-    },
+    }, 
     name: {
         type: DataTypes.STRING,
         allowNull: false
@@ -25,13 +34,10 @@ Band.init({
     end_time: {
         type: DataTypes.DATE,
         allowNull: false
-    },
-}, {
+    }
+  }, {
     sequelize,
     modelName: 'Band',
-    tableName: 'band',
-    timestamps: false
-})
-
-// EXPORT
-module.exports = Bands
+  });
+  return Band;
+};
